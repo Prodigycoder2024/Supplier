153f807a1daa272605b2137cf6fd00c05a86a153
Prodigycoder2024 <gautam.work98@gmail.com>
2024-08-29 16:57:33 +0530

    Created Table for supplier

--- myproject/supplier/urls.py
+++ myproject/supplier/urls.py
@@ -1,10 +1,10 @@
 # supplier/urls.py
 
 from django.urls import path
-from .views import create_supplier
+from .views import *
 
 urlpatterns = [
     # path('', views.supplier_form, name='supplier_form'),
-    path('create-supplier/', create_supplier, name='create_supplier'),
-    # path('update-address/', views.update_address, name='update_address'),
+    path('', create_supplier, name='create_supplier'),
+    path('supplier-details/', supplier_details, name='supplier_details'),
 ]
