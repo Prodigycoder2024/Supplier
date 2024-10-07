3e1862c3ab900a7874a6a1242633b9a76d87b370
Prodigycoder2024 <gautam.work98@gmail.com>
2024-10-07 15:15:24 +0530

    Created View all supplier tab

---
+++ Supplier_Portal/Supplier/urls.py
@@ -0,0 +1,14 @@
+# supplier/urls.py
+
+from django.urls import path
+from .views import *
+from . import views
+
+urlpatterns = [
+    # path('', views.supplier_form, name='supplier_form'),
+    path('', create_supplier, name='create_supplier'),
+    path('supplier-details/', supplier_details, name='supplier_details'),
+    path('create-requisition/', views.create_requisition, name='create_requisition'),
+    path('fetch-suppliers/', views.fetch_and_save_suppliers, name='fetch_and_save_suppliers'),
+    path('suppliers/', views.display_suppliers, name='display_suppliers'),
+]
